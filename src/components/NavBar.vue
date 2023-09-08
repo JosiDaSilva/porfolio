274f1c21919928edf19272c6f9059cbb00bffd98
Josiele Da Silva <josiele@outlook.com>
2023-09-08 19:07:44 -0300

    inicio

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar app color="black" dark flat class="px-12">
+    <v-btn>
+        <v-icon color="yellow" left class="mr-2">fas fa-signature</v-icon>Josiele da Silva
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('inicio')" class="text-yellow">Inicio</v-btn>
+    <v-btn text @click="scroll('about')" >Sobre Mí</v-btn>
+    <v-btn text @click="scroll('portfolio')" >Proyectos</v-btn>
+    <v-btn text @click="scroll('scontacto')" >Contacto</v-btn>
+  </v-app-bar>
+</template>
+<script>
+export default{
+
+}
+</script>
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,73 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <v-app id="home">
+<NavBar/>
+<v-container fluid>
+<div class="head">
+<v-row>
+  <v-col cols="5">
+    <div style="position: relative" class="mt-16">
+    <h1 class="text-grey">Hola,</h1>
+    <h1 class="text-grey">Soy Josiele</h1>
+    <span class="text-grey">Web Designer & Developer</span>
+    <v-btn tile dark class="text-yellow mt-8" variant="outlined">Contáctame</v-btn>
+    </div>
+  </v-col>
+  <v-col cols="7"></v-col>
+</v-row>
+</div>
+</v-container>
+
+ </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
-export default {
+// Components
+import NavBar from '@/components/NavBar.vue';
+
+export default defineComponent({
   name: 'HomeView',
+
   components: {
-    HelloWorld
-  }
-}
+    NavBar
+  },
+});
 </script>
+<style scoped>
+.v-container{
+  padding: 16px 0;
+}
+
+.head{
+ position: relative;
+ text-align: center; 
+ padding: 12px;
+ margin-bottom: 6px;
+ height: 400px;
+ width: 100%;
+ color: white;
+}
+.head:before{
+  content: " ";
+  position: absolute;
+  top: 0;
+  left:0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, 6deg);
+}
+
+.head:after{
+  content: " ";
+  position: absolute;
+  top: 0;
+  right:0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, -6deg);
+}
+
+</style>
